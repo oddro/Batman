a540d82cd93d9e20eff378bb6bb25a2458e43cf2
Arie <arie@wego.com>
2014-03-11 21:35:18 +0700

    Initial Commit

---
+++ VAVA CACA/curveAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  curveAppDelegate.h
+//  VAVA CACA
+//
+//  Created by Flash on 3/11/14.
+//  Copyright (c) 2014 curve7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface curveAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VAVA CACA/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VAVA CACA
+//
+//  Created by Flash on 3/11/14.
+//  Copyright (c) 2014 curve7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "curveAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([curveAppDelegate class]));
+	}
+}
